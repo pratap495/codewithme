a5d4ef3d9e1463d81472623ccc38201e4ca7e16e
pratap495 <20p31a0513@acet.ac.in>
2026-03-16 13:45:39 +0530

    'hero'

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex items-center justify-center h-screen bg-blue-600">
+      <h1 className="text-5xl text-white font-bold">
+        Tailwind is Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
